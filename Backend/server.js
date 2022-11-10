97d1f2aace0ab164810a3f638633f5853daff988
Nadav <nadavvivsa@gmail.com.com>
2022-11-10 19:52:27 +0200

    s

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const http = require('http').createServer(app)
 // Express App Config
 app.use(cookieParser())
 app.use(express.json())
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
 if (process.env.NODE_ENV === 'production') {
     // Express serve static files on production environment
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
     // Configuring CORS
     const corsOptions = {
         // Make sure origin contains the url your frontend is running on
-        origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+        origin: ['http://127.0.0.1:3030', 'http://localhost:5173'],
         credentials: true
     }
     app.use(cors(corsOptions))
@@ -45,12 +45,12 @@ setupSocketAPI(http)
 // Make every server-side-route to match the index.html
 // so when requesting http://localhost:3030/index.html/wap/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue-router to take it from there
-app.get('/**', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+// app.get('/**', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
+// })
 
 const logger = require('./services/logger.service.js')
-const port = process.env.PORT || 5173
+const port = process.env.PORT || 3030
 http.listen(port, () => {
     logger.info('Server is running on port: ' + port)
 })
