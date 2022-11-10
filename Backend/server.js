ff2fb5bdad851390a0c01fc245429e28f9b89fd8
Nadav <nadavvivsa@gmail.com.com>
2022-11-10 19:28:08 +0200

    a

--- Backend/server.js
+++ Backend/server.js
@@ -8,16 +8,16 @@ const http = require('http').createServer(app)
 // Express App Config
 app.use(cookieParser())
 app.use(express.json())
-app.use(express.static('dist'))
+app.use(express.static('public'))
 
 if (process.env.NODE_ENV === 'production') {
     // Express serve static files on production environment
-    app.use(express.static(path.resolve(__dirname, 'dist')))
+    app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
     // Configuring CORS
     const corsOptions = {
         // Make sure origin contains the url your frontend is running on
-        origin: ['http://127.0.0.1:3030', 'http://localhost:5173'],
+        origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
         credentials: true
     }
     app.use(cors(corsOptions))
@@ -46,7 +46,7 @@ setupSocketAPI(http)
 // so when requesting http://localhost:3030/index.html/wap/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue-router to take it from there
 app.get('/**', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 const logger = require('./services/logger.service.js')
