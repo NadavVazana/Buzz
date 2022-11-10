ee9aafb970f2c0882679bb62632779e8d5836b1b
Nadav <nadavvivsa@gmail.com.com>
2022-11-10 18:44:50 +0200

    a

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
     // Express serve static files on production environment
-    app.use(express.static(path.resolve(__dirname, 'public')))
+    app.use(express.static(path.resolve(__dirname, 'dist')))
 } else {
     // Configuring CORS
     const corsOptions = {
@@ -45,9 +45,9 @@ setupSocketAPI(http)
 // Make every server-side-route to match the index.html
 // so when requesting http://localhost:3030/index.html/wap/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue-router to take it from there
-// app.get('/**', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 const logger = require('./services/logger.service.js')
 const port = process.env.PORT || 3030
